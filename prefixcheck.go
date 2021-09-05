20415e96fe3cde53b20601d094ebe4edc8fa8106
Douglas <douglas.secco@gmail.com>
2021-09-05 19:17:44 -0300

    check if ip address is public, private, reserved

--- prefixcheck.go
+++ prefixcheck.go
@@ -7,11 +7,13 @@ import (
 	"sync"
 	"flag"
 	"github.com/projectdiscovery/mapcidr"
+	"github.com/xgfone/netaddr"
 )
 
 type Options struct {
 	PrefixFile			string
 	TargetFile			string
+	IpAddress			string
 	Verbose				bool
 }
 
@@ -25,12 +27,11 @@ func sliceContainsElement(slice []string, element string) bool {
 	return retval
 }
 
-
-
 func parseOptions() *Options {
 	options := &Options{}
 	flag.StringVar(&options.PrefixFile, 		"p", "", "File containing the list of cidr prefixes related to the target")
 	flag.StringVar(&options.TargetFile, 		"t", "", "List of single ip addresses that will be checked against those cidr prefixes")
+	flag.StringVar(&options.IpAddress, 			"r", "", "Check if provided ip address is reserved/private or public (v4)")
 	flag.BoolVar(&options.Verbose, 				"v", false, "Verbose mode for debug purposes")
 
 	flag.Parse()
@@ -56,45 +57,66 @@ func main() {
 	options := parseOptions()
 	var wg sync.WaitGroup
 
-	if options.Verbose == true{
-		fmt.Println("PrefixCheck is running")
-	}
-
-	prefixfilestream, _ := ioutil.ReadFile(options.PrefixFile)
-	prefixcontent := string(prefixfilestream)
-	listofprefixes := strings.Split(prefixcontent, "\n")
-
-	if options.Verbose == true {
-		fmt.Printf("[*] Prefixes loaded: %d \n",len(listofprefixes))
-	}
-	
-	targetfilestream, _ := ioutil.ReadFile(options.TargetFile)
-	targetfilecontent := string(targetfilestream)
-	listoftargetips := strings.Split(targetfilecontent, "\n")
-
-	if options.Verbose == true{
-		fmt.Printf("[*] Target ip address loaded: %d \n",len(targetfilecontent))
-	}
-	
-	
-	for _, prefix := range listofprefixes {
-		for _, ipaddr := range listoftargetips {
+	if options.PrefixFile != "" {
 			
-			if strings.Split(ipaddr, ".")[0] == strings.Split(prefix, ".")[0] {
-				iplist, err := expandCidr(prefix)
-				if err == nil {
-					if options.Verbose == true{
-						fmt.Printf("[*] Checking pair: %s and %s\n",ipaddr,prefix)
-					}
-					wg.Add(1)
-					go checkCidrAddress(options.Verbose, ipaddr, iplist, &wg)
-				} 
+
+		prefixfilestream, _ := ioutil.ReadFile(options.PrefixFile)
+		prefixcontent := string(prefixfilestream)
+		listofprefixes := strings.Split(prefixcontent, "\n")
+
+		if options.Verbose == true {
+			fmt.Printf("[*] Prefixes loaded: %d \n",len(listofprefixes))
+		}
+		
+		targetfilestream, _ := ioutil.ReadFile(options.TargetFile)
+		targetfilecontent := string(targetfilestream)
+		listoftargetips := strings.Split(targetfilecontent, "\n")
+
+		if options.Verbose == true{
+			fmt.Printf("[*] Target ip address loaded: %d \n",len(targetfilecontent))
+		}
+		
+		
+		for _, prefix := range listofprefixes {
+			for _, ipaddr := range listoftargetips {
+				
+				if strings.Split(ipaddr, ".")[0] == strings.Split(prefix, ".")[0] {
+					iplist, err := expandCidr(prefix)
+					if err == nil {
+						if options.Verbose == true{
+							fmt.Printf("[*] Checking pair: %s and %s\n",ipaddr,prefix)
+						}
+						wg.Add(1)
+						go checkCidrAddress(options.Verbose, ipaddr, iplist, &wg)
+					} 
+				}
 			}
 		}
-	}
-	
+		
 
-	wg.Wait()
+		wg.Wait()
+	} 
+	
+	if options.IpAddress != "" {
+		ipv4 := netaddr.MustNewIPAddress(options.IpAddress)
+		if ipv4.IsIPv4() {
+			if ipv4.IsLoopback() {
+				fmt.Printf("%s:loopback\n",options.IpAddress)
+			}
+			if ipv4.IsPrivate() {
+				fmt.Printf("%s:private\n",options.IpAddress)
+			} else {
+				fmt.Printf("%s:public\n",options.IpAddress)
+			}
+			if ipv4.IsReserved() {
+				fmt.Printf("%s:reserved\n",options.IpAddress)
+			}
+		} else {
+			fmt.Printf("%s:invalid\n",options.IpAddress)
+		}
+
+
+	}
 }
 
 
