f81044dffa149957c419a0d9a02d1c8b873cb0c1
Douglas <douglas.secco@gmail.com>
2021-10-05 19:04:31 -0300

    teste

--- pkg/runner/prefixcheck2.go
+++ pkg/runner/prefixcheck2.go
@@ -1,22 +1,13 @@
-package main
+package prefixcheck
 
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"strings"
 	"sync"
 
 	"github.com/projectdiscovery/mapcidr"
-	"github.com/xgfone/netaddr"
 )
-
-type Options struct {
-	PrefixFile			string
-	TargetFile			string
-	IpAddress			string
-	Verbose				bool
-}
 
 /*
 func sliceContainsElement(slice []string, element string) bool {
@@ -76,62 +67,3 @@ func checklist(listoftargetips []string,prefix string, wg sync.WaitGroup, option
 		}
 	}
 }
-
-func main() {
-	options := parseOptions()
-	var wg sync.WaitGroup
-
-	if options.PrefixFile != "" {
-
-		prefixfilestream, _ := ioutil.ReadFile(options.PrefixFile)
-		prefixcontent := string(prefixfilestream)
-		listofprefixes := strings.Split(prefixcontent, "\n")
-
-		if options.Verbose == true {
-			fmt.Printf("[*] Prefixes loaded: %d \n",len(listofprefixes))
-		}
-		
-		targetfilestream, _ := ioutil.ReadFile(options.TargetFile)
-		targetfilecontent := string(targetfilestream)
-		listoftargetips := strings.Split(targetfilecontent, "\n")
-
-		if options.Verbose == true{
-			fmt.Printf("[*] Target ip address loaded: %d \n",len(targetfilecontent))
-		}
-		
-		for _, prefix := range listofprefixes {
-			wg.Add(1)
-			go checklist(listoftargetips,prefix,wg, options)
-
-		}
-		wg.Wait()
-	} 
-
-	if options.IpAddress != "" {
-		ipv4 := netaddr.MustNewIPAddress(options.IpAddress)
-		if ipv4.IsIPv4() {
-			if ipv4.IsLoopback() {
-				fmt.Printf("%s:loopback\n",options.IpAddress)
-			} else {
-				if ipv4.IsReserved() {
-					fmt.Printf("%s:reserved\n",options.IpAddress)
-				} else {
-
-					if ipv4.IsPrivate() {
-						fmt.Printf("%s:private\n",options.IpAddress)
-					} else {
-						fmt.Printf("%s:public\n",options.IpAddress)
-					}
-				}
-			}
-			
-		} else {
-			fmt.Printf("%s:invalid\n",options.IpAddress)
-		}
-
-
-	}
-}
-
-
-
