b6b4c326a80542db391720a10ad78472c42253b9
Douglas <douglas.secco@gmail.com>
2022-02-14 10:25:27 -0300

    mode

--- cmd/prefixcheck/main.go
+++ cmd/prefixcheck/main.go
@@ -16,15 +16,18 @@ type Options struct {
 	IpAddress			string
 	Version				bool
 	Verbose				bool
+	Mode				string
+
 }
 
 func parseOptions() *Options {
 	options := &Options{}
-	flag.StringVar(&options.PrefixFile, 		"p", "", "File containing the list of cidr prefixes related to the target")
+	flag.StringVar(&options.PrefixFile, 		"p", "", "File containing the list of cidr prefixes")
 	flag.StringVar(&options.TargetFile, 		"t", "", "List of single ip addresses that will be checked against those cidr prefixes")
 	flag.StringVar(&options.IpAddress, 			"r", "", "Check if provided ip address is reserved/private or public (v4)")
 	flag.BoolVar(&options.Version, 				"i", false, "Version info")
 	flag.BoolVar(&options.Verbose, 				"v", false, "Verbose mode for debug purposes")
+    flag.StringVar(&options.Mode, 				"m", "ip", "Mode (ip|cidr)")
 
 	flag.Parse()
 	return options
@@ -38,15 +41,13 @@ func main() {
 	}
 	
 	if options.PrefixFile != "" {
-
 		prefixfilestream, _ := ioutil.ReadFile(options.PrefixFile)
 		prefixcontent := string(prefixfilestream)
 		listofprefixes := strings.Split(prefixcontent, "\n")
-
 		if options.Verbose == true {
 			fmt.Printf("[*] Prefixes loaded: %d \n",len(listofprefixes))
 		}
-		
+
 		targetfilestream, _ := ioutil.ReadFile(options.TargetFile)
 		targetfilecontent := string(targetfilestream)
 		listoftargetips := strings.Split(targetfilecontent, "\n")
@@ -54,14 +55,28 @@ func main() {
 		if options.Verbose == true{
 			fmt.Printf("[*] Target ip address loaded: %d \n",len(listoftargetips))
 		}
+
+		if options.Mode == "ip" { 
+			
+			for _, prefix := range listofprefixes {
+				wg.Add(1)
+				go prefixcheck.CheckForIp(listoftargetips, prefix, &wg, options.Verbose)
+			}
+			wg.Wait()
+			return
 		
-		for _, prefix := range listofprefixes {
-			wg.Add(1)
-			go prefixcheck.Checklist(listoftargetips, prefix, &wg, options.Verbose)
 		}
-		
-		wg.Wait()
-	} 
+		if options.Mode == "cidr" { 
+			for _, prefix := range listofprefixes {
+				wg.Add(1)
+				go prefixcheck.CheckForCidr(listoftargetips, prefix, &wg, options.Verbose)
+			}
+			wg.Wait()
+			return
+		}
+
+		fmt.Println("Valid mode??")
+	}
 
 	if options.IpAddress != "" {
 		fmt.Println(prefixcheck.CheckIp(options.IpAddress))
